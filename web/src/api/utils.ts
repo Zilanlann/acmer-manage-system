aeb7b2378c9a18b5124029dbfdbef9d1ffaff426
Zilanlann <666zilan666@gmail.com>
2024-04-16 22:45:56 +0800

    build: change backend url

--- web/src/api/utils.ts
+++ web/src/api/utils.ts
@@ -1,2 +1,4 @@
-export const baseUrlApi = (url: string) => `/api/${url}`;
-export const v1BaseUrlApi = (url: string) => `/api/v1/${url}`;
+export const baseUrlApi = (url: string) =>
+  `https://ams-server.fly.dev/api/${url}`;
+export const v1BaseUrlApi = (url: string) =>
+  `https://ams-server.fly.dev/api/v1/${url}`;
